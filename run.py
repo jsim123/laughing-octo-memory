df35f5f2d5638c4d830fdb57623b02aaaab7f955
Johnson <z5214110@student.unsw.edu.au>
2019-01-29 19:27:03 +1100

    added syncing for play and pause

--- run.py
+++ run.py
@@ -1,9 +1,28 @@
 from app import app
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO,send, emit
+
 socketio = SocketIO(app)
 
 import routes
 
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+
+@socketio.on('play')
+def handle_played(json):
+    print('Video has' + str(json))
+    socketio.emit('playVid',broadcast=True)
+
+
+
+@socketio.on('pause')
+def handle_paused(json):
+    print('Video has' + str(json))
+    socketio.emit('pauseVid',broadcast=True)
+
+
 if __name__ == '__main__':
 
-	socketio.run(app,port=3000)
+    socketio.run(app,port=3000)
