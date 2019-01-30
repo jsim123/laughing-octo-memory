70a0f45760a50143d8754583eb91b34a761f49e3
Johnson <z5214110@student.unsw.edu.au>
2019-01-30 20:28:33 +1100

    added seeking but has bugs

--- run.py
+++ run.py
@@ -22,6 +22,11 @@ def handle_paused(json):
     print('Video has' + str(json))
     socketio.emit('pauseVid',broadcast=True)
 
+@socketio.on('buffering')
+def handle_buffer(json):
+    print('Video has buffered at' + str(json))
+    socketio.emit('buffervid',json,broadcast=True)
+
 
 if __name__ == '__main__':
 
